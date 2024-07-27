9fa874f5d8953ee2aa525ea41ddde539719c485e
Your Name <naimur202164@gmail.com>
2024-07-27 16:12:13 +0600

    code

---
+++ index.py
@@ -0,0 +1,5 @@
+print("""hello world
+      
+      
+      helllo you are my love  
+      """)
---
+++ input.py
@@ -0,0 +1,7 @@
+a=input("Enter a Number of 1 to 10")
+
+
+b=input("enter number 2")
+
+print("Number a is:",a)
+print("Number b is:",b)
